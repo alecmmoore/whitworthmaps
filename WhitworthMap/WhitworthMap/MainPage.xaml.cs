aefd477bf847c804740cef207bffc3abe927a05c
Alec Moore <amoore@sixthavestudios.com>
2014-01-27 15:09:47 -0800

    such changes

--- WhitworthMap/WhitworthMap/MainPage.xaml.cs
+++ WhitworthMap/WhitworthMap/MainPage.xaml.cs
@@ -12,25 +12,30 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Geolocation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace WhitworthMap
 {
     
     public sealed partial class MainPage : Page
     {
+
+        //private MobileServiceCollection<Event, Event> events;
+        private IMobileServiceTable<Event> eventsTable = App.MobileService.GetTable<Event>();
+
         public MainPage()
         {
             this.InitializeComponent();
-            Get_Coord();
+            GetCoord();
         }
-        
-        public async void Get_Coord() 
+
+        public async void GetCoord() 
         { 
             //creates object of geolocator type
             Geolocator geolocator = new Geolocator();
-           //creates geopostion object, makes requests every 10 seconds for f minutes
+            //creates geopostion object, makes requests every 10 seconds for f minutes
             Geoposition geopostion = await geolocator.GetGeopositionAsync(maximumAge: TimeSpan.FromMinutes(5), timeout: TimeSpan.FromSeconds(10));
             double LatIt = geopostion.Coordinate.Latitude;
             double LongIt = geopostion.Coordinate.Longitude;
@@ -42,22 +47,39 @@ namespace WhitworthMap
             Canvas.SetLeft(locationPing, Calc2 - 15);
             Canvas.SetTop(locationPing, Calc1 - 15);
         }
-        
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-			
-        }
 
         private void BackButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
         {
-        	// TODO: Add event handler implementation here.
             ViewBuildings.Begin();
         }
 
         private void Title_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
         {
-        	// TODO: Add event handler implementation here.
             ViewEvents.Begin();
         }
+
+        private async void Building_Tapped(object sender, RoutedEventArgs e)
+        {
+            var key = Regex.Match((sender as FrameworkElement).Name, @"^.*?(?=_)").ToString();
+
+            var query = await eventsTable
+                .Where(o => o.Locations.Contains(key))
+                .Select(o => o)
+                .ToCollectionAsync();
+
+            if (query.Count() > 0)
+            {
+                EventList.ItemsSource = query;
+                NoEvents.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                EventList.ItemsSource = null;
+                NoEvents.Visibility = Visibility.Visible;
+            }
+
+            ViewEvents.Begin();
+        }
+
     }
 }
